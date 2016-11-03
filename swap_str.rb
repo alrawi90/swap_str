497e19680b58a1b895a6a4d3badd47417698e48e
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-03 12:32:20 +0000

    done

---
+++ swap_str.rb
@@ -0,0 +1,17 @@
+def swap_str(str="Hi There , iTS CoLd day")
+  new_str=[]
+arr=str.split(" ")
+arr.each do |word|
+  word.capitalize
+  word.split("").each do |letter|
+    if letter==letter.upcase
+      new_str.push(letter.downcase)
+    else
+      new_str.push(letter.upcase)
+    end
+  end
+  new_str.push(" ")
+end
+new_str.join("")
+end
+puts swap_str
